f4d99d87d9ce897eba70b80ed23b3a6fb52f3f89
max-bergen <maxbergen1234@gmail.com>
2017-03-29 23:06:17 +0000

    wat

--- db/seeds/options.js
+++ db/seeds/options.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('poll').del()
     .then(function () {
       return Promise.all([
-        knex.insert({option: 'chill', description: 'chill hard', submit_count: 1}).into('options')
+        knex.insert({option: 'chill', description: 'chill hard', poll_id: 1, submit_count: 1}).into('options')
       ]);
     });
 };

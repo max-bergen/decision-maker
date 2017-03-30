ab530ee5fa929cbb5729476c87d6403356a11112
max-bergen <maxbergen1234@gmail.com>
2017-03-30 01:26:53 +0000

    made addtopoll and addtooptions functions

--- db/seeds/options.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex, Promise) {
-  return knex('poll').del()
-    .then(function () {
-      return Promise.all([
-        knex.insert({option: 'chill', description: 'chill hard', poll_id: 1, submit_count: 1}).into('options')
-      ]);
-    });
-};

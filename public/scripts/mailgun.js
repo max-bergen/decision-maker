4c8de393f8eca978ddb055ad2d6d4959721d731f
Couragyn Chretien <couragyn@gmail.com>
2017-03-30 17:45:50 +0000

    added mailgun functionality that sends admin links to poll after submit

--- public/scripts/mailgun.js
+++ public/scripts/mailgun.js
@@ -3,13 +3,13 @@ var domain = 'sandbox2a9b32fa5d5340d196ac1b14ec5933ef.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 
-module.exports = (recipient) => {
+module.exports = (recipient, title, admin, user) => {
 
   var data = {
-    from: 'decision-maker@gmail.com',
+    from: 'Link-Generator@Decision-maker.com',
     to: recipient,
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
+    subject: `${title} Poll Created`,
+    text: `Administrative Link: http://localhost:8080/${admin}\nVoter Link: http://localhost:8080/${user}`
   };
 
   mailgun.messages().send(data, function (error, body) {

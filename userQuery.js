08b4e0d07662f36d8fae0fd86d49502aeb8f7d8c
max-bergen <maxbergen1234@gmail.com>
2017-03-31 19:40:02 +0000

    created updateSubmitCount and FindSubmitCount functions

--- userQuery.js
+++ userQuery.js
@@ -4,25 +4,31 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 
 module.exports = function queryPoll(userUrl) {
-  knex.select('id', 'title').from('poll')
-  .where('user_url', '=', userUrl)
-  .asCallback(function(err, rows) {
-  if (err) return console.error(err);
-      queryOptions({id: rows[0].id, title: rows[0].title});
- })
-}
-
-function queryOptions(pollObj) {
-let optionsArr = [];
-knex.select('option', 'description').from('options')
-.where('poll_id', '=', pollObj.id)
-.asCallback(function(err, rows) {
-  if (err) return console.error(err);
-      rows.forEach(function(key){
-      let optionsObj = {option: key.option, description: key.description, title: pollObj.title};
-      optionsArr.push(optionsObj);
+ return new Promise((resolve, reject) => {
+    knex.select('id', 'title').from('poll')
+      .where('user_url', '=', userUrl)
+      .asCallback(function(err, rows) {
+        if (err) return reject(err);
+        let optionObj = queryOptions({id: rows[0].id, title: rows[0].title});
+        optionObj.then(function(result) {
+          resolve(result);
+        })
       })
-      return optionsArr;
   })
 }
 
+function queryOptions(pollObj) {
+  return new Promise((resolve, reject) => {
+    let optionsArr = [];
+    knex.select('option', 'description').from('options')
+      .where('poll_id', '=', pollObj.id)
+      .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        rows.forEach(function(key){
+          let optionsObj = {pollID: pollObj.id, optionID: key.id, option: key.option, description: key.description, title: pollObj.title};
+          optionsArr.push(optionsObj);
+      })
+      resolve(optionsArr);
+    })
+  })
+}

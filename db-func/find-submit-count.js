fa1f932d8ef19ffc0b2b108900a493ff6144bd8e
max-bergen <maxbergen1234@gmail.com>
2017-04-01 18:31:15 +0000

    moving db functions into dir called db-fun

---
+++ db-func/find-submit-count.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const ENV         = process.env.ENV || "development";
+const knexConfig  = require("../knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+
+module.exports = function findSubmitCount(optionId) {
+  let submitCountArr = [];
+  return new Promise((resolve, reject) => {
+    knex.select('*').from('options')
+    .where('id', '=', optionId)
+    .asCallback(function(err, rows) {
+      if (err) return reject(err);
+      resolve (rows[0].submit_count);
+   })
+  });
+}
---
+++ db-func/user-query.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const ENV         = process.env.ENV || "development";
+const knexConfig  = require("../knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+
+module.exports = function queryPoll(userUrl) {
+  return new Promise((resolve, reject) => {
+    knex.select('id', 'title').from('poll')
+      .where('user_url', '=', userUrl)
+      .asCallback(function(err, rows) {
+        if (err) return reject(err);
+        let optionObj = queryOptions({id: rows[0].id, title: rows[0].title});
+        optionObj.then(function(result) {
+          resolve(result);
+        })
+      })
+  })
+}
+
+function queryOptions(pollObj) {
+  return new Promise((resolve, reject) => {
+    let optionsArr = [];
+    knex.select('id', 'option', 'description').from('options')
+      .where('poll_id', '=', pollObj.id)
+      .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        rows.forEach(function(key){
+          let optionsObj = {pollID: pollObj.id, optionID: key.id, option: key.option, description: key.description, title: pollObj.title};
+          optionsArr.push(optionsObj);
+      })
+      resolve(optionsArr);
+    })
+  })
+}

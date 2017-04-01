005094813d64f8405165eeddf92d746bc6c82a7b
max-bergen <maxbergen1234@gmail.com>
2017-04-01 18:35:58 +0000

    moved db funcitons to folder called db-func and removed their individual requires

--- db-func/update-submit-count.js
+++ db-func/update-submit-count.js
@@ -1,8 +1,3 @@
-require('dotenv').config();
-const ENV         = process.env.ENV || "development";
-const knexConfig  = require("../knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-
 module.exports = function updateSubmitCount(id, newCount) {
 
   knex('options')
